1449b0c57b68279de6a721b2dbb7ec01597b1b43
18003 <miaojiankang>
2021-03-02 11:01:22 +0800

    Halfway to update

--- PythonWord编程/main1.py
+++ PythonWord编程/main1.py
@@ -2,5 +2,5 @@
 from docx import Document
 
 Doc = Document()
-Doc.add_heading("Python是什么东西？？？")
-print("saldkjf")
+Doc.add_heading("Python是什么东西？？？") # 添加标题
+Doc.add_paragraph("Python是一种面向对象的编程语言~~~") # 添加段落
